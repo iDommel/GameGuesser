b902100e79522aa3a0be5a89259181b221e753a3
404_Patate <71853641+Carasssiusaurat@users.noreply.github.com>
2024-01-11 23:52:18 +0100

    sheeeeeeeeeeeeeeeeeeeeeeeeeesh

--- gameguesserfront/src/App.tsx
+++ gameguesserfront/src/App.tsx
@@ -1,42 +1,216 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import logo from './pizza.jpeg';
 import './App.css';
-import { Select } from 'antd';
+import { Select, Card } from 'antd';
+import { define } from 'mime';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Unstable_Grid2';
+import CachedIcon from '@mui/icons-material/Cached';
+import IconButton from '@mui/material/IconButton';
+import { error } from 'console';
+
+interface gameTypeDb {
+  appId: number,
+  categories: string[],
+  developers: string[],
+  tags: string[],
+  name: string,
+  publishers: string[],
+  release: string | number,
+  price: string,
+  requiredAge: number,
+  score: number | null,
+}
+
+interface gameType extends gameTypeDb {
+  label: string,
+  value: string,
+  key: number,
+}
+
+enum answerNumber{
+  inferior,
+  superior,
+  equal,
+}
+
+interface answerArrayType {
+  answer: boolean
+  data: string
+}
+
+interface answerNumberType {
+  answer : answerNumber
+  data: number
+}
+
+type gameTypeAnswer = {
+  appId: number,
+  categories: answerArrayType[],
+  developers: answerArrayType[],
+  tags: answerArrayType[],
+  name: string,
+  publishers: answerArrayType[],
+  release: string | number,
+  price: string,
+  requiredAge: answerNumberType,
+  score: number | null,
+  isWin: boolean
+}
+
+type FillCardLabelsProps = {
+  label: answerArrayType[];
+  labelName: string;
+  isWin: boolean;
+};
+
+type FillCardNumbersProps = {
+  label: answerNumberType;
+  labelName: string;
+  isWin: boolean;
+};
+const compArray = (data: string[], compData: string[], label: string): answerArrayType[] => {
+  let answer: answerArrayType[] = []
+
+  if (data === undefined){
+    console.log("data undefined ", label)
+    return []
+  } else if (compData === undefined) {
+    console.log("compData undefined")
+    return []
+  }
+  data.forEach(element => {
+    if (compData.includes(element)) {
+      answer.push({answer: true, data: element})
+    } else {
+      answer.push({answer: false, data: element})
+    }
+  });
+
+  return answer
+}
+
+const compGamesNumbers = (data: number, compNumber: number): answerNumberType => {
+  let answer: answerNumberType = {answer: answerNumber.equal, data: data}
+  if (data < compNumber) {
+    answer.answer = 0
+  } else if (data > compNumber) {
+    answer.answer = 1
+  }
+  return answer
+}
 
 function App() {
-  const countries = [
-    { label: "Belgium", value: "Europe", key: 0 },
-    { label: "India", value: "Asia", key: 1 },
-    { label: "Bolivia", value: "South America", key: 2 },
-    { label: "Ghana", value: "Africa", key: 3},
-    { label: "Japan", value: "Asia", key: 4 },
-    { label: "Canada", value: "North America", key: 5 },
-    { label: "New Zealand", value: "Australasia", key: 6 },
-    { label: "Italy", value: "Europe", key: 7 },
-    { label: "South Africa", value: "Africa", key: 8 },
-    { label: "China", value: "Asia", key: 9 },
-    { label: "Paraguay", value: "South America", key: 10 },
-    { label: "Usa", value: "North America", key: 11 },
-    { label: "France", value: "Europe", key: 12 },
-    { label: "Botswana", value: "Africa", key: 13 },
-    { label: "Spain", value: "Europe", key: 14},
-    { label: "Senegal", value: "Africa", key: 15},
-    { label: "Brazil", value: "South America", key: 16},
-    { label: "Denmark", value: "Europe", key: 17 },
-    { label: "Mexico", value: "South America", key: 18 },
-    { label: "Australia", value: "Australasia", key: 19},
-    { label: "Tanzania", value: "Africa", key: 20 },
-    { label: "Bangladesh", value: "Asia", key: 21 },
-    { label: "Portugal", value: "Europe", key: 22 },
-    { label: "Pakistan", value: "Asia", key: 23 }
-  ];
-
-  const [searchGame, setSearchGame] = useState("")
-  const [selectedGames, setSelectedGames] = useState<any[]>([""])
   
-  const onChange = (value: string, option: { label: string; value: string; key: number; } | { label: string; value: string; key: number; }[]) => {
-    console.log(`selected ${value}  key = ${(option as { label: string; value: string; key: number; }).key}`);
-    setSelectedGames([...selectedGames, value]);
+  const [searchGame, setSearchGame] = useState("")
+  const [selectedGames, setSelectedGames] = useState<gameTypeAnswer[] | undefined>(undefined)
+  const [gameToguess, setGameToGuess] = useState<gameType | undefined>(undefined)
+  const [gamesData, setGamesData] = useState<gameType[]>([])
+  
+  const getGames = async () => {
+    const dbData = await fetch('http://localhost:8000/games')
+    const gamesJson = await dbData.json()
+    const formattedData: gameType[]= []
+
+    for (let index = 0; index < gamesJson.length; index++) {
+      const id = gamesJson[index].appId as number
+      const data: gameType = {
+        appId: gamesJson[index].appId,
+        categories: gamesJson[index].categories,
+        developers: gamesJson[index].developers,
+        tags: gamesJson[index].tags,
+        name: gamesJson[index].name,
+        publishers: gamesJson[index].publishers,
+        release: gamesJson[index].release,
+        price: gamesJson[index].price,
+        requiredAge: gamesJson[index].requiredAge,
+        score: gamesJson[index].score,
+        label: gamesJson[index].name,
+        value: gamesJson[index].name,
+        key: index,
+      }
+      formattedData.push(data)
+    }
+    console.log("formatted data : ", formattedData)
+    setGamesData(formattedData)
+  }
+
+  const getGameToGuess = async () => {
+    const dbData = await fetch('http://localhost:8000/get_current_game')
+    const gameJson = await dbData.json()
+    console.log("game to guess : ", gameJson)
+    setGameToGuess(gameJson)
+  }
+
+  const randomizeAndGetNewGame = async () => {
+    await fetch('http://localhost:8000/randomize_current_game').then(
+      response => response.json()
+    ).then(data => {
+      console.log(data)
+      getGameToGuess()
+    }
+    ).catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    getGames()
+    getGameToGuess()
+  }, [])
+  
+  const compGames = (searchGame: gameType) => {
+    console.log("searchGame : ", searchGame.categories)
+    if (gameToguess != undefined && searchGame.name.toLowerCase() === gameToguess.name.toLowerCase()) {
+      console.log("win")
+      const answerData: gameTypeAnswer = {
+        appId: searchGame.appId,
+        categories: compArray(searchGame.categories, gameToguess.categories, "categories"),
+        developers: compArray(searchGame.developers, gameToguess.developers, "devs"),
+        tags: compArray(searchGame.tags, gameToguess.tags, "tags"),
+        name: searchGame.name,
+        publishers: compArray(searchGame.publishers, gameToguess.publishers, "publishers"),
+        release: searchGame.release,
+        price: searchGame.price,
+        requiredAge: compGamesNumbers(Number(searchGame.release), Number(gameToguess.release)),
+        score: searchGame.score,
+        isWin: true
+      }
+      return answerData
+    } else if (gameToguess != undefined) {
+      const answerData: gameTypeAnswer = {
+        appId: searchGame.appId,
+        categories: compArray(searchGame.categories, gameToguess.categories, "categories"),
+        developers: compArray(searchGame.developers, gameToguess.developers, "devs"),
+        tags: compArray(searchGame.tags, gameToguess.tags, "tags"),
+        name: searchGame.name,
+        publishers: compArray(searchGame.publishers, gameToguess.publishers, "publishers"),
+        release: searchGame.release,
+        price: searchGame.price,
+        requiredAge: compGamesNumbers(Number(searchGame.release), Number(gameToguess.release)),
+        score: searchGame.score,
+        isWin: false
+      }
+      console.log("answerData : ", answerData)
+      return answerData
+    }
+  }
+
+  const onChange = (value: string, option: gameType | gameType[]) => {
+    console.log(`selected ${value}  key = ${(option as gameType).key} release : ${(option as gameType).price}`);
+    console.log("searchGame : ", (option as gameType).appId)
+    const gameAnswer = compGames(option as gameType)
+    
+    if (selectedGames == undefined) {
+      if (gameAnswer != undefined) {
+        setSelectedGames([gameAnswer]);
+      }
+    } else {
+      if (gameAnswer != undefined) {
+        setSelectedGames([...selectedGames, gameAnswer]);
+      }
+
+    }
   };
   
   const onSearch = (label: string) => {
@@ -47,9 +221,29 @@ function App() {
   // Filter `option.label` match the user type `input`
   const filterOption = (input: string, option?: { label: string; value: any }) =>
     (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+  
+  const FillCardLabels = ({label, labelName, isWin}: FillCardLabelsProps) => {
+    return (
+      <Card title={labelName} bordered={false} style={isWin ? {backgroundColor:"green", flexDirection:"row"} : {backgroundColor:"white", flexDirection:"row"}}>
+        {label.map((item) => (
+          <p style={item.answer ? {backgroundColor:"green", margin: 5} : {backgroundColor:"red", margin: 5}}>{item.data}</p>
+        ))}
+      </Card>
+    )
+  }
+
+  const FillCardNumbers = ({label, labelName, isWin}: FillCardNumbersProps) => {
+    return (
+      <Card title={labelName} style={{flexDirection:"row"}}>
+        <p style={label.answer === answerNumber.equal ? {backgroundColor:"green", margin: 5} : {backgroundColor:"red", margin: 5}}>{label.data.toString()}</p>
+      </Card>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <AppBar position="static">
+        <Toolbar variant="dense">
         <Select
           showSearch
           placeholder="Select a person"
@@ -57,13 +251,32 @@ function App() {
           onChange={onChange}
           onSearch={onSearch}
           filterOption={filterOption}
-          options={countries}
+          options={gamesData}
+          style={{alignSelf:"auto"}}
         />
+        <IconButton
+            onClick={randomizeAndGetNewGame}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            style={{marginLeft:10}}
+          >
+            <CachedIcon/>
+          </IconButton>
+        <div style={{flexDirection:"row"}}>
+        </div>
+        </Toolbar>
+      </AppBar>
+      <header className="App-header">
         <div style={{flexDirection:"column"}}>
-          {selectedGames.map((game, index) => (
-            <div>
-            <text>{game}</text>
-            </div>
+          {selectedGames === undefined ? <p>oui</p> : selectedGames.slice(0).reverse().map((game: gameTypeAnswer, index:number) => (
+            <Card title={game.name} bordered={true} style={game.isWin ? styles.winCard : styles.loseCard} key={index}>
+              <FillCardLabels label={game.categories} labelName={"categories"} isWin={game.isWin}/>
+              <FillCardLabels label={game.developers} labelName={"developers"} isWin={game.isWin}/>
+              <FillCardLabels label={game.publishers} labelName={"publishers"} isWin={game.isWin}/>
+            </Card>
           ))}
         </div>
       </header>
@@ -71,5 +284,20 @@ function App() {
   );
 }
 
+const styles = {
+  loseCard: {
+    width: 800,
+    margin: 40,
+    borderColor: "red",
+    borderWidth:2
+  },
+  winCard: {
+    width: 800,
+    margin: 40,
+    borderColor: "green",
+    borderWidth:2,
+    backgroundColor: "green"
+  }
+}
 export default App;
 
